0edb8f633fab68fdef1b2715dabfbeda9c178a6c
unknown <hectoralonso650@gmail.com>
2023-08-14 11:17:15 -0700

    added code for get_game_id

--- server/app.py
+++ server/app.py
@@ -20,7 +20,7 @@ def get_all_games():
     api_key = "46e2b52b8c314c51916eab168dc92f87"
     params = {
         "key": api_key,
-        "page_size": 1000,
+        "page_size": 350000,
     }
     
     # Make the API request and get the response data
@@ -43,35 +43,30 @@ def get_all_games():
         games.append(game_data)
     
     return jsonify(games)
-
+@app.route('/games/<int:id>')
+def get_game_id(id):
+    # Using f-string to retrieve specific game's endpoint based on the id
+    api_url = f"https://api.rawg.io/api/games/{id}"
+    api_key = "46e2b52b8c314c51916eab168dc92f87"
+    params = {
+        "key": api_key,
+        "page_size": 350000,
+    }
+    response = requests.get(api_url, params=params)
     
-
-# '/users' is an endpoint with method
-# @app.route('/users', methods=["GET", "POST"])
-# def users():
-#     print("users endpoint reached..")
-#     if requests.method == "GET":
-#         # r = reading
-#         with open("users.json", "r") as f:
-#             data = json.load(f)
-#             data.append({
-#                 "username": "user4",
-#                 "pets": ["hamster"]
-#             })
-
-#             # with a newly added user data set
-#             # have to send it as a valid response
-#             # we have to convert data agin using flask.jsonify()
-#             return flask.jsonify(data)
-#     if request.method == "POST":
-#         received_data = request.get_json()
-#         print(f"received data: {received_data}")
-#         message = received_data['data']
-#         return_data = {
-#             "status": "success",
-#             "message": f"recieved: {message}"
-#         }
-#         return flask.Response(response=json.dumps(return_data), status=201)
+    if response.status_code == 200:
+        response_json = response.json()
+        game_data ={
+            'id': response_json['id'],
+            'name': response_json['name'],
+            'description': response_json['description'],
+            'background_image_additional': response_json['background_image_additional'],
+            'rating': response_json['rating'],
+            'metacritic': response_json['metacritic']
+        }
+        return jsonify(game_data)
+    
+    
 
 if __name__ == "__main__":
     app.run("localhost", 6969)
